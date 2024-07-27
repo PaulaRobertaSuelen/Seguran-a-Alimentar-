b9fd52b5639b124343cd884a7a9f4f0955fe1508
PaulaRobertaSuelen <paularobertasuelen27@gmail.com>
2024-07-27 14:01:46 -0300

    feature section 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
 
+import Home  from "./pages/Home/home";
+
 
 export default function App() {
 
   return (
     <>
         <RouterProvider router={router}/>
+        <Home/>
     </>
   )
 }
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom"
 import Navbar from "../../components/Navbar/navbar"
+import Main from "../../components/main/Main"
+import Section1 from "./components/section1"
 
-export default function Home( ){
-    const navigation = useNavigate()
-
-    return (
+export default function Main(){
+    return(
         <>
-            <Navbar/>
+        <Navbar/>
           {/** esse foi um botao que eu criei com ananias */}
         <button onClick={() => navigation('/login')}>login</button>
+        <Main/>
+        <Section1/>
         </>
+
     )
 }
