bb7297ca44147f853f584146016b05a0b1cde7c0
Itala <samaraitala18@gmail.com>
2024-08-04 00:42:56 -0300

    alteraçao de designer

--- src/pages/PesquisarPerfil/index.jsx
+++ src/pages/PesquisarPerfil/index.jsx
@@ -21,22 +21,25 @@ export default function Pesquisar(){
                                 <img src="src/assets/logoSemLetra.png" alt=" Logo" />
                             </S.Cabercario>
 
-                           <S.Resultado>
-                                <p>Resultados:</p>
-                           </S.Resultado>
-                           
-                           <div>
-                                <h2> Entenda a seletividade alimentar</h2>
-                                <p>Tags:</p>
-                                <img src="src/assets/criaça.png" alt="imagen de uma criança " />
-                                <input type="text" placeholder="Comportamento alimentar" />
-                                <input type="text" placeholder="Sensibilidade"/>
-                                <img src="src/assets/bola(1).png" alt="bolha" />
-                                <img src="src/assets/bola(2) .png" alt="bolha" />
-                                <p>Conteúdo:</p>
-                                <p> A seletividade alimentar é comum em crianças com Transtorno do Espectro  Autista (TEA). Ela se caracteriza pela recusa em experimentar novos  alimentos, um repertório restrito de alimentos aceitos e resistência a pratos novos...</p>
-                                <button>Ver mais </button>
-                           </div>
+                            <S.Resultado>
+
+                            </S.Resultado>
+                                    <p> Resultado </p>
+                            <S.Container>
+                                    <S.Caixa>
+
+                                    <h2> Entenda a seletividade alimentar</h2>
+                                    <p>Tags:</p>
+                                    <S.Im src="src/assets/criaça.png" alt="imagen de uma criança " />
+                                    <input type="text" placeholder="Comportamento alimentar" />
+                                    <input type="text" placeholder="Sensibilidade"/>
+                                    <S.Im src="src/assets/bola(1).png" alt="bolha" />
+                                    <S.Im src="src/assets/bola(2) .png" alt="bolha" />
+                                    <p>Conteúdo:</p>
+                                    <p> A seletividade alimentar é comum em crianças com Transtorno do Espectro  Autista (TEA). Ela se caracteriza pela recusa em experimentar novos  alimentos, um repertório restrito de alimentos aceitos e resistência a pratos novos...</p>
+                                    <button>Ver mais </button>
+                                </S.Caixa>
+                            </S.Container>
 
 
                 </>
--- src/pages/PesquisarPerfil/styles.js
+++ src/pages/PesquisarPerfil/styles.js
@@ -2,18 +2,71 @@ import styled from 'styled-components';
 
 export const Cabercario = styled.div`
     display: flex;
-    align-items: center;
-    font-size: 20px;
     justify-content: space-around;
-    padding: 10px;
+    align-items: center;
+    padding: 20px;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        margin-right: 20px;
+
+        input {
+        margin-top: 5px;
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        }
+    }
+        /* butao de Buscar */
+    button {
+        border: 2px solid #33B44D;
+        background-color: rgb( #DEEDF5);
+        border-radius: 10px;
+        font-size: 20px;
+        margin-top: 30px;
+        padding: 5px;
+    }
+
 `;
 export const Resultado = styled.div`
+    margin: 20px;
+    display: flex;
+    margin-right: 20px;
+
+    
+`;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    
+`; 
+export const Caixa = styled.div`
+    width: 80%;
+    padding: 20px;
+    margin: 10px 0;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    background-color: #f9f9f9;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    h2{
+        font-size: 24px;
+        margin-bottom: 10px;
+    }
+`;
+
+export const Im = styled.img`
+    width: 14rem;
+
+`;
+export const Corpo = styled.div`
+
 display: flex;
-align-items:  center;
-font-size: 18px;
+align-items: baseline;
+font-size: 20px;
 justify-content: space-between;
-
-
 `;
 
 
