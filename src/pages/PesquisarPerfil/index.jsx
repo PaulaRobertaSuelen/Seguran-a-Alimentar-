6724d135dfb1dafb3d9d98b5d2758f4994264f9d
Itala <samaraitala18@gmail.com>
2024-08-05 00:54:22 -0300

    Segunda alteração de designer

--- src/pages/PesquisarPerfil/index.jsx
+++ src/pages/PesquisarPerfil/index.jsx
@@ -21,22 +21,22 @@ export default function Pesquisar(){
                                 <img src="src/assets/logoSemLetra.png" alt=" Logo" />
                             </S.Cabercario>
 
-                            <S.Resultado>
-
-                            </S.Resultado>
-                                    <p> Resultado </p>
+                            
+                                <S.Resultado> Resultados:</S.Resultado>
+                    
                             <S.Container>
                                     <S.Caixa>
 
                                     <h2> Entenda a seletividade alimentar</h2>
-                                    <p>Tags:</p>
+                                    <h1>Tags:</h1>
                                     <S.Im src="src/assets/criaça.png" alt="imagen de uma criança " />
                                     <input type="text" placeholder="Comportamento alimentar" />
                                     <input type="text" placeholder="Sensibilidade"/>
                                     <S.Im src="src/assets/bola(1).png" alt="bolha" />
                                     <S.Im src="src/assets/bola(2) .png" alt="bolha" />
-                                    <p>Conteúdo:</p>
-                                    <p> A seletividade alimentar é comum em crianças com Transtorno do Espectro  Autista (TEA). Ela se caracteriza pela recusa em experimentar novos  alimentos, um repertório restrito de alimentos aceitos e resistência a pratos novos...</p>
+                                    <h1>Conteúdo:</h1>
+                                    <p> A seletividade alimentar é comum em crianças com Transtorno do Espectro  Autista (TEA). 
+                                        Ela se caracteriza pela recusa em experimentar novos  alimentos, um repertório restrito de alimentos aceitos e resistência a pratos novos...</p>
                                     <button>Ver mais </button>
                                 </S.Caixa>
                             </S.Container>
--- src/pages/PesquisarPerfil/styles.js
+++ src/pages/PesquisarPerfil/styles.js
@@ -29,11 +29,13 @@ export const Cabercario = styled.div`
     }
 
 `;
-export const Resultado = styled.div`
-    margin: 20px;
-    display: flex;
-    margin-right: 20px;
-
+export const Resultado = styled.p`
+    margin-left: 140px; //afasta do canto
+    padding: 10px 20px ;
+    color: wheat;
+    background-color: #7499E8;
+    width: 120px;
+    border-radius: 20px;
     
 `;
 export const Container = styled.div`
@@ -54,7 +56,41 @@ export const Caixa = styled.div`
     h2{
         font-size: 24px;
         margin-bottom: 10px;
+        margin-left: 300px
     }
+    p{
+        font-size: 24px;
+        margin-bottom: 10px;
+        margin-left: 400px;
+    }
+    h1{
+        font-size: 24px;
+        margin-bottom: 10px;
+        margin-left: 300px
+    }
+    input {
+    width: (20% - 12px);
+    padding: 5px;
+    border: 1px solid #33B579;
+    border-radius: 10px;
+    background-color: #33B579;
+    justify-content: space-between;
+    
+
+    
+
+    }
+    button {
+        border: 2px solid #33B44D; 
+        background-color: #33B44D; 
+        color: #f9f9f9;
+        font-size: 20px;
+        border-radius: 20px; //aredonda
+        margin-left: 600px;
+        padding: 10px;
+    }
+
+    
 `;
 
 export const Im = styled.img`
@@ -62,11 +98,9 @@ export const Im = styled.img`
 
 `;
 export const Corpo = styled.div`
-
-display: flex;
-align-items: baseline;
-font-size: 20px;
-justify-content: space-between;
-`;
+    align-items: baseline;
+    font-size: 20px;
+    justify-content: space-between;
+    `;
 
 
