b1bf428197143a329fc070edf5b6af07128c4d24
Itala <samaraitala18@gmail.com>
2024-08-15 23:40:07 -0300

    fix: importação do footer

--- Front/src/pages/Home/index.jsx
+++ Front/src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 //import React from 'react';
 
 import Navbar from '../../components/Navbar/navbar';
-import Footer from '../../components/Footer/footer';
+import Footer from '../../components/Footer/Footer';
 
 import Logo from '../../assets/logo.png';
 import OndaUm from '../../assets/onda-um.png';
