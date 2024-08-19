ac2557b1cb1caeb026f52d6ed7102dca92fbc408
Asyal <laysaelisabeth0101@gmail.com>
2024-08-19 18:39:21 -0300

    style: ajustar a indentação do código para consistência

--- Front/src/pages/Home/index.jsx
+++ Front/src/pages/Home/index.jsx
@@ -1,145 +1,133 @@
-import React from "react";
+import React from 'react';
 
-import Navbar from "../../components/Navbar/navbar"
-import Footer from "../../components/Footer/Footer"
+import Navbar from '../../components/Navbar/navbar';
+import Footer from '../../components/Footer/Footer';
 
-import Logo from "../../assets/logo.png"
-import OndaUm from "../../assets/onda-um.png"
-import OndaDois from "../../assets/onda-dois.png"
-import Mother from "../../assets/mother.png"
+import Logo from '../../assets/logo.png';
+import OndaUm from '../../assets/onda-um.png';
+import OndaDois from '../../assets/onda-dois.png';
+import Mother from '../../assets/mother.png';
 
-import * as S from "./styles";
+import * as S from './styles';
 
 export default function Home() {
-  return (
-    <>
+    return (
+        <>
+            {/* A NAVBAR PRECISA SER INCLUÍDA AQUI ANTES DO BODY COMO HEADER */}
 
-    {/* A NAVBAR PRECISA SER INCLUÍDA AQUI ANTES DO BODY COMO HEADER */}
+            <Navbar />
 
-    <Navbar />
+            {/* ============================================================================================== */}
+            {/* IMAGEM INICIAL COM A LOGO, O TÍTULO E O BOTÃO DE "SAIBA MAIS" */}
 
+            <S.BackgroundContainer>
+                <S.OverlayContent>
+                    <S.Logo>
+                        <img src={Logo} alt="logo" />
+                    </S.Logo>
 
-    {/* ============================================================================================== */}
-      {/* IMAGEM INICIAL COM A LOGO, O TÍTULO E O BOTÃO DE "SAIBA MAIS" */}
+                    <S.TituloEBotao>
+                        <S.TituloUm>
+                            <h1>Refeições sem desafios!</h1>
+                        </S.TituloUm>
 
-      <S.BackgroundContainer>
+                        <S.BotaoSaibaMais>Saiba mais</S.BotaoSaibaMais>
+                    </S.TituloEBotao>
+                </S.OverlayContent>
+            </S.BackgroundContainer>
 
-        <S.OverlayContent>
+            {/* ============================================================================================== */}
+            {/* AQUI ESTÁ COMO CORPO DA HOME PARA PODER ATRIBUIR UMA COR DE FUNDO */}
 
-          <S.Logo>
-            <img src={Logo} alt="logo" />
-          </S.Logo>
+            <S.Body>
+                {/* ============================================================================================== */}
+                {/* PARTE DO "IREMOS DE AJUDAR" */}
 
-          <S.TituloEBotao>
+                <S.IremosAjudar>
+                    <S.TituloIremosTeAjudar>
+                        <h1>Iremos te ajudar</h1>
+                    </S.TituloIremosTeAjudar>
 
-            <S.TituloUm>
-              <h1>Refeições sem desafios!</h1>
-            </S.TituloUm>
+                    <S.TextoDeAjuda>
+                        <p>
+                            Grande parte das pessoas que possuem o TEA tem a{' '}
+                            <span>seletividade alimentar</span>, podendo gerar
+                            desafios nas horas das refeições e impactando na
+                            saúde.
+                        </p>
+                        <p>
+                            A equipe do BemTEA visa ajudar nessa situação e
+                            melhorar vidas.
+                        </p>
+                    </S.TextoDeAjuda>
 
-            <S.BotaoSaibaMais>
-              Saiba mais
-            </S.BotaoSaibaMais>
+                    <S.BotaoQueroComecar>Quero começar</S.BotaoQueroComecar>
+                </S.IremosAjudar>
 
-          </S.TituloEBotao>
+                {/* ============================================================================================== */}
+                {/* PARTE DO "COMO FUNCIONA" */}
 
-        </S.OverlayContent>
+                <S.ComoFunciona>
+                    <S.TituloComoFunciona>
+                        <img src={OndaUm} alt="onda-um" />
+                        <h1>Como funciona?</h1>
+                        <img src={OndaDois} alt="onda-dois" />
+                    </S.TituloComoFunciona>
 
-      </S.BackgroundContainer>
+                    <S.PassoAPasso>
+                        <S.PassoUm>
+                            <S.TextoPasso>
+                                <h1>1</h1>
+                                <p>Procure</p>
+                            </S.TextoPasso>
+                        </S.PassoUm>
 
-    {/* ============================================================================================== */}
-    {/* AQUI ESTÁ COMO CORPO DA HOME PARA PODER ATRIBUIR UMA COR DE FUNDO */}
+                        <S.PassoDois>
+                            <S.TextoPasso>
+                                <h1>2</h1>
+                                <p>Encontre</p>
+                            </S.TextoPasso>
+                        </S.PassoDois>
 
-    <S.Body>
+                        <S.PassoTres>
+                            <S.TextoPasso>
+                                <h1>3</h1>
+                                <p>Entenda</p>
+                            </S.TextoPasso>
+                        </S.PassoTres>
+                    </S.PassoAPasso>
+                </S.ComoFunciona>
 
-      {/* ============================================================================================== */}
-      {/* PARTE DO "IREMOS DE AJUDAR" */}
+                {/* ============================================================================================== */}
+                {/* PARTE DO "NUTRIÇÃO É PARA TODOS" */}
 
-      <S.IremosAjudar>
+                <S.NutricaoParaTodos>
+                    <S.NutricaoTitulo>
+                        <h1>Nutrição é para todos</h1>
+                        <p>
+                            O BemTEA foi projetado para facilitar o acesso de
+                            famílias com crianças atípicas com informações sobre
+                            nutrição e estratégias para descomplicar as horas
+                            das refeições. Acesse guias, quadros e depoimentos e
+                            experiências em outros perfis.
+                        </p>
+                    </S.NutricaoTitulo>
 
-        <S.TituloIremosTeAjudar>
-          <h1>Iremos te ajudar</h1>
-        </S.TituloIremosTeAjudar>
+                    <S.NutricaoImagem>
+                        <img src={Mother} alt="mae-e-filha" />
+                    </S.NutricaoImagem>
+                </S.NutricaoParaTodos>
+            </S.Body>
 
-        <S.TextoDeAjuda>
-          <p>Grande parte das pessoas que possuem o TEA tem a <span>seletividade alimentar</span>, podendo gerar desafios nas horas das refeições e impactando na saúde.</p>
-          <p>A equipe do BemTEA visa ajudar nessa situação e melhorar vidas.</p>
-        </S.TextoDeAjuda>
+            {/* ============================================================================================== */}
+            {/* FIM DO BODY */}
 
-        <S.BotaoQueroComecar>
-          Quero começar
-        </S.BotaoQueroComecar>
+            <Footer />
 
-      </S.IremosAjudar>
+            {/* ============================================================================================== */}
+            {/* AQUI ABAIXO DO BODY DEVE SER INCLUÍDO O FOOTER QUANDO ESTIVER PRONTO */}
 
-      {/* ============================================================================================== */}
-      {/* PARTE DO "COMO FUNCIONA" */}
-
-      <S.ComoFunciona>
-
-        <S.TituloComoFunciona>
-
-          <img src={OndaUm} alt="onda-um" />
-          <h1>Como funciona?</h1>
-          <img src={OndaDois} alt="onda-dois" />
-
-        </S.TituloComoFunciona>
-
-        <S.PassoAPasso>
-
-          <S.PassoUm>
-            <S.TextoPasso>
-              <h1>1</h1>
-              <p>Procure</p>
-            </S.TextoPasso>
-          </S.PassoUm>
-
-          <S.PassoDois>
-            <S.TextoPasso>
-              <h1>2</h1>
-              <p>Encontre</p>
-            </S.TextoPasso>
-          </S.PassoDois>
-
-          <S.PassoTres>
-            <S.TextoPasso>
-              <h1>3</h1>
-              <p>Entenda</p>
-            </S.TextoPasso>
-          </S.PassoTres>
-
-        </S.PassoAPasso>
-
-      </S.ComoFunciona>
-
-      {/* ============================================================================================== */}
-      {/* PARTE DO "NUTRIÇÃO É PARA TODOS" */}
-
-      <S.NutricaoParaTodos>
-
-        <S.NutricaoTitulo>
-          <h1>Nutrição é para todos</h1>
-          <p>O BemTEA foi projetado para facilitar o acesso de famílias com crianças atípicas com informações sobre nutrição e estratégias para descomplicar as horas das refeições. Acesse guias, quadros e depoimentos e experiências em outros perfis.</p>
-        </S.NutricaoTitulo>
-
-        <S.NutricaoImagem>
-          <img src={Mother} alt="mae-e-filha" />
-        </S.NutricaoImagem>
-
-      </S.NutricaoParaTodos>
-
-    </S.Body>
-
-    {/* ============================================================================================== */}
-    {/* FIM DO BODY */}
-
-    <Footer />
-
-    {/* ============================================================================================== */}
-    {/* AQUI ABAIXO DO BODY DEVE SER INCLUÍDO O FOOTER QUANDO ESTIVER PRONTO */}
-
-    {/* ============================================================================================== */}
-
-
-    </>
-  );
+            {/* ============================================================================================== */}
+        </>
+    );
 }
--- Front/src/routes/index.jsx
+++ Front/src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Login from '../pages/Login/login.jsx';
-import Home from '../pages/Home';
+import Home from '../pages/Home/index.jsx';
 import Redefinirsenha from '../pages/Redefinirsenha';
 
 export const router = createBrowserRouter([
